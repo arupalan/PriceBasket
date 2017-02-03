2ffbeff4b97f767dd40c0061ae17e42deb38297a
Arup Banerjee <arup.banerjee@emacc-net.local>
2017-02-03 15:35:02 +0000

    Initial Commit

---
+++ PriceBasket.Service/PriceBasketService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBasket.Service
+{
+    partial class PriceBasketService : ServiceBase
+    {
+        public PriceBasketService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StartUp();
+        }
+
+        private void StartUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStop()
+        {
+            Shutdown();
+        }
+
+        private void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PriceBasket.Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBasket.Service
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var service = new PriceBasketService();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}

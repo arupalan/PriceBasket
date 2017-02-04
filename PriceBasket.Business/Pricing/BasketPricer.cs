9c0e91eda18a68db7d76d6e31f645abf395292e4
arupalan <arupalan@gmail.com>
2017-02-04 13:47:01 +0000

    Added DI Framework

---
+++ PriceBasket.Business/Pricing/BasketPricer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriceBasket.Business.Models;
+
+namespace PriceBasket.Business.Pricing
+{
+    public class BasketPricer
+    {
+
+        public readonly IBasketEconomicsManager BasketEconomicsManager;
+
+        public BasketPricer(IBasketEconomicsManager basketEconomicsManager)
+        {
+            this.BasketEconomicsManager = basketEconomicsManager;
+        }
+    }
+}

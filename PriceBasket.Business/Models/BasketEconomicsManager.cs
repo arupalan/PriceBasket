4b6c4e1c20597796639508571c84f805d7cb868c
arupalan <arupalan@gmail.com>
2017-02-04 12:40:47 +0000

    Added Models

---
+++ PriceBasket.Business/Models/BasketEconomicsManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBasket.Business.Models
+{
+    public class BasketEconomicsManager
+    {
+        private readonly ConcurrentDictionary<string, BasketItemEconomics> basketItemEconomicses;
+
+        public BasketEconomicsManager()
+        {
+            basketItemEconomicses = new ConcurrentDictionary<string, BasketItemEconomics>();
+        }
+        public bool TryAddItem(string itemName, BasketItemEconomics item)
+        {
+            return true;
+        }
+    }
+}
---
+++ PriceBasket.Business/Models/BasketItemEconomics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceBasket.Business.Models
+{
+    public class BasketItemEconomics : IEqualityComparer<BasketItemEconomics>
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+
+        public bool Equals(BasketItemEconomics x, BasketItemEconomics y)
+        {
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(BasketItemEconomics obj)
+        {
+            BasketItemEconomics bie = (BasketItemEconomics)obj;
+            return bie.Name.GetHashCode();
+        }
+    }
+}
